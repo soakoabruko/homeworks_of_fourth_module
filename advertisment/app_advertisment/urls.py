b0d6237fb7aad75646ca1e66cbae16bdf0e7c13b
Перцовый <soakoabruko@gmail.com>
2023-07-19 13:24:55 +0300

    Домашняя работа пятого урока

--- advertisment/app_advertisment/urls.py
+++ advertisment/app_advertisment/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers, advertisment_post, register, login, profile
 
 urlpatterns = [
-    path( '', index ),
-    path( 'start', index )
+    path( '', index, name = 'main_page' ),
+    path( 'top-sellers/', top_sellers, name = 'top_sellers' ),
+    path( 'advertisment-post/', advertisment_post , name = 'advertisment_post' ),
+    path( 'register/', register, name = 'register' ),
+    path( 'login/', login, name = 'login' ),
+    path( 'profile/', profile, name = 'profile' )
 ]
--- advertisment/app_lesson_4/urls.py
+++ advertisment/app_lesson_4/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import answer
 
 urlpatterns = [
-    path( 'lesson_4', answer )
+    path( '', answer )
 ]
