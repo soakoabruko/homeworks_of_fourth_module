5a5e879e3c3907b5d686845e7d580e9cb5263943
Перцовый <soakoabruko@gmail.com>
2023-08-06 16:39:10 +0300

    Домашняя работа седьмого урока.

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Advertisment( models.Model ):
+    title = models.CharField( 'Название', max_length = 100 )
+    description = models.TextField( 'Описание' )
+    price = models.DecimalField( 'Цена', max_digits = 10, decimal_places = 2 )
+    auction = models.BooleanField( 'Торг', help_text = 'Отметьте, если торг уместен.' )
+    created_at = models.DateTimeField( auto_now_add = True )
+    updated_at = models.DateTimeField( auto_now_add = True )
+
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__( self ):
+        return f'Advertisment( id = { self.id }, title = { self.title }, price = { self.price } )'
